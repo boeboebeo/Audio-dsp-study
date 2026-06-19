52ec5b606962f43971e02abb6a97b0c29abc45bd
boe <boeboebeo@gmail.com>
2026-06-20 02:01:08 +0900

    feat : z_transform

--- DSP-basics/day11-01_Z-transform.py
+++ DSP-basics/day11-01_Z-transform.py
@@ -13,3 +13,11 @@ Understand discrete-time filters using Z-domain analysis
 (Z영역을 이용한 이산시간 필터 분석 이해)
 
 """
+
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy import signal
+from scipy.fft import fft, fftfreq
+
+SAMPLE_RATE = 44100
+
